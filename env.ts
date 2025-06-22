00aa657ad29f3f15bca7f30cfe2f1f0e1af9d3a8
Dariusz Filipiak <darekf77@gmail.com>
2025-06-22 22:39:41 +0200

    first

---
+++ env.ts
@@ -0,0 +1,6 @@
+import type { EnvOptions } from "tnp/src";
+
+const env: Partial<EnvOptions> = {
+  website: { domain: "piano-cli.example.domain.com", title: "Piano Cli" },
+};
+export default env;
---
+++ src/lib/build-info._auto-generated_.ts
@@ -0,0 +1,7 @@
+// THIS FILE IS GENERATED - DO NOT MODIFY
+export const BUILD_FRAMEWORK_CLI_NAME = 'tnp';
+export const APP_ID = 'com.domain.example.piano-cli';
+export const PROJECT_NPM_NAME = 'piano-cli';
+export const CURRENT_PACKAGE_VERSION = '0.0.0';
+// THIS FILE IS GENERATED - DO NOT MODIFY
+      
